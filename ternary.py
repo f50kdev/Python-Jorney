337053e956d7118deb4e81c9f3d56d20534c5f46
f50kdev <f50kdev@example.com>
2025-10-23 15:54:13 +0530

    ternary

---
+++ ternary.py
@@ -0,0 +1,13 @@
+# inserting value into variable
+
+name = " Faustino"
+age = 22
+
+if age != 22 :
+  message = "true ";
+else:
+  message = "not true"
+
+  #simplifing to ternary
+
+  message = "true value" if age != 20  else "tblw nao verdadeoro";s
